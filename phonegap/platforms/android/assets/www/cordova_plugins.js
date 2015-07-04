4789857dba2bdbd939f8e6e3d0b8bca7ea9552df
Dariusz Niemczyk <palid@o2.pl>
2015-07-04 15:22:54 +0200

    BLE.

--- phonegap/platforms/android/assets/www/cordova_plugins.js
+++ phonegap/platforms/android/assets/www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.megster.cordova.bluetoothserial/www/bluetoothSerial.js",
+        "id": "com.megster.cordova.bluetoothserial.bluetoothSerial",
+        "clobbers": [
+            "window.bluetoothSerial"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.megster.cordova.bluetoothserial": "0.4.3"
 }
 // BOTTOM OF METADATA
 });
--- phonegap/platforms/android/assets/www/js/index.js
+++ phonegap/platforms/android/assets/www/js/index.js
@@ -16,34 +16,58 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
+var UUID = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  onSuccesfulBle: function(e) {
+    this.ble = true;
+    bluetoothSerial.write('jestem ziemniakiem :D');
+  },
+  onFailBle: function(e) {
+    this.ble = false;
+  },
+  setupBle: function() {
+    bluetoothSerial.connect(UUID, this.onSuccesfulBle.bind(this), this.onFailBle.bind(this));
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicitly call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    var self = this;
+    app.receivedEvent('deviceready');
+    document.querySelector('#boom').addEventListener('click', function(e) {
+      self.explode();
+      e.preventDefault();
+      return false;
+    });
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+    console.log('Received Event: ' + id);
+  },
+  explode: function() {
+    if (!ble) {
+      this.setupBle();
     }
+    this.exploded = true;
+  }
 };
